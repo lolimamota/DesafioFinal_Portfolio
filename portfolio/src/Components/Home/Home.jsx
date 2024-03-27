8b6486aeb89108983084404c8dc4160b38b6f1aa
Lorrayne <lorraynelimamota@gmail.com>
2024-03-27 20:59:55 -0300

    start: estilização da home

--- portfolio/src/Components/Home/Home.jsx
+++ portfolio/src/Components/Home/Home.jsx
@@ -4,14 +4,14 @@ import * as S from  './StyleHome.jsx';
 
 function Home() {
   return (
-    <>
+    <S.Main>
     <h1>CONHEÇA A CAPITÃ DESSA AVENTURA:</h1>
     <S.Perfil src={Perfil} alt="Foto de perfil de Lorrayne" />
     <button>Nome</button>
     <button>Habilidades1</button>
     <button>Habilidades2</button>
     <button>FavThings</button>
-    </>
+    </S.Main>
   )
 }
 
--- portfolio/src/Components/Home/StyleHome.jsx
+++ portfolio/src/Components/Home/StyleHome.jsx
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
 export const Perfil = styled.img`
-width: 400px;
+width: 250px;
 border-radius: 50%;
+`;
+
+export const Main = styled.main`
+color: white;
+display: grid;
+grid-template-columns: repeat(3, 2fr);
+grid-template-rows: repeat(4, 2fr);
 `;
