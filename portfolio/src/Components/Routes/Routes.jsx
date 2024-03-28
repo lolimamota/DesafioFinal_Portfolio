c0d9ef6278359a4ae26729352881e35c034f2381
Lorrayne <lorraynelimamota@gmail.com>
2024-03-27 21:48:07 -0300

    Start: estilização routes

--- portfolio/src/Components/Routes/Routes.jsx
+++ portfolio/src/Components/Routes/Routes.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import Home from '../Home/Home.jsx'
-import About from '../About/About.jsx'
-import Projects from '../Projects/Projects.jsx'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from '../Home/Home.jsx';
+import About from '../About/About.jsx';
+import Projects from '../Projects/Projects.jsx';
+import * as S from './StyleRoute.jsx';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 export default function Router(){
     return (
         <BrowserRouter>
-            <nav>
-                <ul>
+            <S.Nav>
+                <S.Menu>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/About">About</Link></li>
                     <li><Link to="/Projects">Projects</Link></li>
-                </ul>
-            </nav>
+                </S.Menu>
+            </S.Nav>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/About" element={<About/>}/>
---
+++ portfolio/src/Components/Routes/StyleRoute.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+height: 10vh;
+`;
+
+export const Menu = styled.ul`
+border: solid red;
+width:100%;
+height: 10vh;
+display: flex;
+justify-content: space-around;
+align-items: center;
+`;
