214721c1a626b86bfa2bd313e06b0cd7feaa9256
Lorrayne <lorraynelimamota@gmail.com>
2024-04-07 15:44:42 -0300

    ANIMAÇÃO OK

--- portfolio/src/Components/About/AboutRoute/MapPage.jsx
+++ portfolio/src/Components/About/AboutRoute/MapPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import * as S from '../StyleAbout.jsx';
 
 import Passos from '../img/pezinho.png';
@@ -7,25 +7,52 @@ import Passos from '../img/pezinho.png';
 function MapPage() {
   let navigate = useNavigate();
 
-  const handleClickMBACK = ()=>navigate(-1);
+  const handleClickMBACK = () => navigate(-1);
+
   return (
     <>
-    <section>
-    <S.BtnBack onClick={()=>handleClickMBACK()}>VOLTAR</S.BtnBack >
-    <S.H2>Não é de hoje que a tecnologia está presente...</S.H2>
-    </section>
-    <S.Text>
-    <S.Paragraph>Em 2012, fiz um curso de jogos digitais, tendo meu primeiro contato com programação lá...</S.Paragraph>
-    <S.Paragraph>...Confesso que não foi amor a primeira vista e por muitos anos, o IF e o ELSE assombravam meus sonhos rsrsrs</S.Paragraph>
-    <S.Paragraph>Mas a tech não desistiu de mim, e seja atráves dos amigos ou conhecimentos aleatórios, ela vez ou outra voltava pra minha vida!</S.Paragraph>
-    <S.Paragraph>E chegamos em 2023, comigo pulando de corpo e alma nos aprendizados de diversas linguagens tecnologicas, vamos conhecer elas?</S.Paragraph>
-    </S.Text>
-    <S.PegadasAnimadas>
-      <img src={Passos} alt="Marca de pegadas" />
-      <img src={Passos} alt="Marca de pegadas" />
-      <img src={Passos} alt="Marca de pegadas" />
-      <img src={Passos} alt="Marca de pegadas" />
-    </S.PegadasAnimadas>
+      <section>
+        <S.BtnBack onClick={() => handleClickMBACK()}>VOLTAR</S.BtnBack >
+        <S.H2>Não é de hoje que a tecnologia está presente...</S.H2>
+      </section>
+      <S.Text>
+        <S.Paragraph>Em 2012, fiz um curso de jogos digitais, tendo meu primeiro contato com programação lá...</S.Paragraph>
+        <S.Paragraph>...Confesso que não foi amor a primeira vista e por muitos anos, o IF e o ELSE assombravam meus sonhos rsrsrs</S.Paragraph>
+        <S.Paragraph>Mas a tech não desistiu de mim, e seja atráves dos amigos ou conhecimentos aleatórios, ela vez ou outra voltava pra minha vida!</S.Paragraph>
+        <S.Paragraph>E chegamos em 2023, comigo pulando de corpo e alma nos aprendizados de diversas linguagens tecnologicas, vamos conhecer elas?</S.Paragraph>
+      </S.Text>
+      <S.PegadasAnimadas className='pezinhos'>
+        <div className="pezinhos-slide">
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+        </div>
+        <div className="pezinhos-slide">
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+        </div>
+        <div className="pezinhos-slide">
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+        </div>
+        <div className="pezinhos-slide">
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+        </div>
+        <div className="pezinhos-slide">
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+          <img src={Passos} alt="Pegadas humanas" />
+        </div>
+      </S.PegadasAnimadas>
     </>
   )
 }
--- portfolio/src/Components/About/StyleAbout.jsx
+++ portfolio/src/Components/About/StyleAbout.jsx
@@ -22,7 +22,7 @@ border-radius: 50px 50px 50px 50px;
 border:none;
 width:15%;
 height: 5vh;
-`; 
+`;
 
 export const H2 = styled.h2`
 text-align: center;
@@ -39,7 +39,7 @@ align-items: center;
 export const Paragraph = styled.p`
 width: 20%;
 height: 25vh;
-text-align: justify;
+text-align: center;
 border: solid 0.5px #6d72c3;
 border-radius: 5px;
 &:hover{
@@ -53,17 +53,70 @@ padding: 10vh;
 `;
 
 export const PegadasAnimadas = styled.section`
-height: 15vh;
-border: solid red;
-display: flex;
-justify-content: space-around;
-align-items: center;
-&:hover{
-    animation: to left 0.5s ease-out;
-}
-img{
+/* width: 100%;
+display: inline-block;
+white-space: nowrap;
+overflow: hidden; */
+
+/* img{
     border: solid white;
     width: 80px;
-    display: flex;
+    margin: 0 0.2rem;
+    animation: pegadinhas 1s infinite linear ;
+}
+@keyframes pegadinhas {
+    0%{
+        transform: translateX(0)
+    }
+    100%{
+        transform: translateX(100%)
+    }
+} */
+
+//--------------tentativa de estilização com as classes
+overflow: hidden;
+padding: 1rem 0;
+white-space: nowrap;
+position: relative;
+
+&:before{
+position:absolute;
+top: 0;
+width:40rem;
+height: 100%;
+content: "";
+z-index: 2;
+right:0;
+background-image: linear-gradient(to right , transparent, #230903);
+}
+&:after{
+position:absolute;
+top: 0;
+width:40rem;
+height: 100%;
+content: "";
+z-index: 2;
+left:0;
+background-image: linear-gradient(to left ,  transparent , #230903);
+}
+&:hover .pezinhos-slide{
+    animation-play-state: paused;
+}
+.pezinhos-slide{
+    animation: 20s slide infinite linear;
+    display: inline-flex;
+}
+.pezinhos-slide img{
+    height: 80px;
+    margin: 0 55px;
+}
+@keyframes slide {
+    0%{
+        transform: translateX(0);
+    }
+    100%{
+        transform: translateX(100%)
+    }
 }
 `;
+
