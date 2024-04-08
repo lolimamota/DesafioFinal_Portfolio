b9e01fc36829ef716d4fd155a2138b53003fb9f7
Lorrayne <lorraynelimamota@gmail.com>
2024-04-07 22:10:17 -0300

    desenvolvimento da aba developer

--- portfolio/src/Components/About/AboutRoute/Developer.jsx
+++ portfolio/src/Components/About/AboutRoute/Developer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom';
+import * as S from '../StyleAbout.jsx'
 
 function Developer() {
   let navigate = useNavigate();
@@ -7,7 +8,13 @@ function Developer() {
   const handleClickMBACK = ()=>navigate(-1);
   return (
     <>
-    <button onClick={()=>handleClickMBACK()}>VOLTAR</button>
+    <S.BtnBack onClick={()=>handleClickMBACK()}>VOLTAR</S.BtnBack>
+    <p>
+      A proxima etapa é cloud, em breve concluirei a minha formação no Vai na Web(a primeira de muitas diga-se de passagem!), agora se com este portfólio, você viu o potencial dessa Sandy(&Junior) e quer me ver na equipe, é só entrar em contato comigo atraves das minhas redes!
+    </p>
+    {/* <img src={GitHub} alt="Logo do github" />
+    <img src={LinkedIn} alt="logo do LinkedIn" />
+    <img src={WhatsApp} alt="Logo do WhatsApp" /> */}
     </>
   )
 }
