c8fb936e48f6665830adbbd6774771889e824119
Lorrayne <lorraynelimamota@gmail.com>
2024-04-10 20:24:31 -0300

    add a targets and object on array

--- portfolio/src/Components/About/AboutRoute/Developer.jsx
+++ portfolio/src/Components/About/AboutRoute/Developer.jsx
@@ -17,15 +17,15 @@ function Developer() {
       A proxima etapa é cloud, em breve concluirei a minha formação no Vai na Web (a primeira de muitas diga-se de passagem!), agora se com este portfólio, você viu o potencial dessa Sandy(&Junior) e quer me ver na equipe, é só entrar em contato comigo atraves das minhas redes!
     </S.TextTech2>
     <S.DivDev>
-      <S.AnchorTag href="https://github.com/lolimamota">
+      <S.AnchorTag href="https://github.com/lolimamota" target="_blank">
         <img src={GitHub} alt="Logo do github" />
       </S.AnchorTag>
 
-    <S.AnchorTag href="https://www.linkedin.com/in/lorraynelimamota/">
+    <S.AnchorTag href="https://www.linkedin.com/in/lorraynelimamota/" target="_blank">
       <img src={LinkedIn} alt="logo do LinkedIn" />
     </S.AnchorTag>
 
-    <S.AnchorTag href="https://api.whatsapp.com/send/?phone=%2B5531992539210&text&type=phone_number&app_absent=0">
+    <S.AnchorTag href="https://api.whatsapp.com/send/?phone=%2B5531992539210&text&type=phone_number&app_absent=0" target="_blank">
     <img src={WhatsApp} alt="Logo do WhatsApp" />
     </S.AnchorTag>
     </S.DivDev>
--- portfolio/src/Components/Projects/Projects.jsx
+++ portfolio/src/Components/Projects/Projects.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Fantastika from './img/cao.png';
-import CN from './img/cartoon.png';
+import Cartoon from './img/cartoon.png';
 import Cruzeiro from './img/cruzeiro.png';
 import TheWitcher from './img/fogo.png';
 import Parceiros  from './img/patrocinadores.png';
 
 function Projects() {
   const Challenges = [
-    {image: Fantastika , title: Fantastika , link:"https://fantastikapetshop.vercel.app/" , description:"Página desenvolvida com as seguintes technologias: HTML e CSS, aplicando tecnicas como indentação e responsividade"}
+    {image: Fantastika , title: 'Fantastika' , link:"https://fantastikapetshop.vercel.app/" , description:"Página desenvolvida com as seguintes technologias: HTML e CSS, aplicando tecnicas como indentação e responsividade"},
+    {image: Cartoon , title: 'CartoonNetwork' , link:"https://cn-exercise-veg2.vercel.app/", description:"Página desenvolvida com as seguintes technologias: HTML e CSS, aplicando tecnicas como indentação e responsividade"},
   ]
   return (
     <div>Projects</div>
