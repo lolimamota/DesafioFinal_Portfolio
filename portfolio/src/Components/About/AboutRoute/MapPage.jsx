ba147003ab0eb87c2b108c993aab347f868bda05
Lorrayne <lorraynelimamota@gmail.com>
2024-04-10 22:32:32 -0300

    adjustment

--- portfolio/src/Components/About/AboutRoute/MapPage.jsx
+++ portfolio/src/Components/About/AboutRoute/MapPage.jsx
@@ -19,7 +19,7 @@ function MapPage() {
         <S.Paragraph>Em 2012, fiz um curso de jogos digitais, tendo meu primeiro contato com programação lá...</S.Paragraph>
         <S.Paragraph>...Confesso que não foi amor a primeira vista e por muitos anos, o IF e o ELSE assombravam meus sonhos rsrsrs</S.Paragraph>
         <S.Paragraph>Mas a tech não desistiu de mim, e seja atráves dos amigos ou conhecimentos aleatórios, ela vez ou outra voltava pra minha vida!</S.Paragraph>
-        <S.Paragraph>E chegamos em 2023, comigo pulando de corpo e alma nos aprendizados de diversas linguagens tecnologicas, vamos conhecer elas?</S.Paragraph>
+        <S.Paragraph>E chegamos em 2023, comigo pulando de corpo e alma nos aprendizados de diversas linguagens tecnologicas, vamos conhecer elas? Clique ali em cima pra voltar!</S.Paragraph>
       </S.Text>
       <S.PegadasAnimadas className='pezinhos'>
         <div className="pezinhos-slide">
